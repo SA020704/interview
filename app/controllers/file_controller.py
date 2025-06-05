0c102c969f2cfea6c76e50415b5c0561c057b44d
Evan <EvanSun@88vip.site>
2025-06-05 10:55:15 +0800

    恢复原本的上传接口

--- app/controllers/file_controller.py
+++ app/controllers/file_controller.py
@@ -11,10 +11,8 @@ s3_utils = S3Utils()
 @bp.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
-    # result = s3_utils.upload_file_to_s3(file)
-    res = ALYAudioConversionUtil.file_trans("https://static.datingpaas.net/interview/20250605_102644_e13ea828.wav")
-    return jsonify(Response.ok(res).__dict__)
-    # if result['success']:
-    #     return jsonify(Response.ok(result).__dict__)
-    # else:
-    #     return jsonify(Response.error(f"上传失败: {result['message']}").__dict__), 400
+    result = s3_utils.upload_file_to_s3(file)
+    if result['success']:
+        return jsonify(Response.ok(result).__dict__)
+    else:
+        return jsonify(Response.error(f"上传失败: {result['message']}").__dict__), 400
